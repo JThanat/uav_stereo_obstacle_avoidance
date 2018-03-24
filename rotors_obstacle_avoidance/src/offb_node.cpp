baf4b237952e2870ecf55ff85cfc2fee54c07cf5
JThanat <thanat.jat@gmail.com>
2018-03-24 20:00:50 +0700

    Add simple waypoints

--- rotors_obstacle_avoidance/src/offb_node.cpp
+++ rotors_obstacle_avoidance/src/offb_node.cpp
@@ -9,6 +9,8 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <vector>
+#include <cmath>
 
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr &msg)
@@ -16,34 +18,72 @@ void state_cb(const mavros_msgs::State::ConstPtr &msg)
     current_state = *msg;
 }
 
+geometry_msgs::PoseStamped current_pose;
+void pose_cb(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
+    current_pose = *msg;
+}
+
+bool checkEqualPose(const geometry_msgs::PoseStamped expectedPosition)
+{
+    return (
+        std::abs(expectedPosition.pose.position.x - current_pose.pose.position.x) < 0.05 && 
+        std::abs(expectedPosition.pose.position.y - current_pose.pose.position.y) < 0.05 && 
+        std::abs(expectedPosition.pose.position.z - current_pose.pose.position.z) < 0.05
+    );
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
+    std::vector<geometry_msgs::PoseStamped> poses(4);
+    int i = 0;
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
+
+    ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 10, pose_cb);
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
     // wait for FCU connection
     while (ros::ok() && !current_state.connected)
     {
+        ROS_INFO("current state connected %d", current_state.connected);
         ros::spinOnce();
+        ROS_INFO("SPIN!");
         rate.sleep();
+        ROS_INFO("Wake up");
     }
+    ROS_INFO("set pose array");
 
-    geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 2;
+    // geometry_msgs::PoseStamped pose;
+    // pose.pose.position.x = 0;
+    // pose.pose.position.y = 0;
+    // pose.pose.position.z = 2;
+    poses[0].pose.position.x = 0;
+    poses[0].pose.position.y = 0;
+    poses[0].pose.position.z = 2;
+
+    poses[1].pose.position.x = 0;
+    poses[1].pose.position.y = 5;
+    poses[1].pose.position.z = 2;
+
+    poses[2].pose.position.x = 5;
+    poses[2].pose.position.y = 5;
+    poses[2].pose.position.z = 2;
+
+    poses[3].pose.position.x = 5;
+    poses[3].pose.position.y = 0;
+    poses[3].pose.position.z = 2;
 
     //send a few setpoints before starting
     for (int i = 100; ros::ok() && i > 0; --i)
     {
-        local_pos_pub.publish(pose);
+        local_pos_pub.publish(poses[0]);
         ros::spinOnce();
         rate.sleep();
     }
@@ -58,11 +98,12 @@ int main(int argc, char **argv)
 
     while (ros::ok())
     {
+        ROS_INFO("ros ok!");
         if (current_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0)))
         {
             if (set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent
+                offb_set_mode.response.mode_sent)
             {
                 ROS_INFO("Offboard enabled");
             }
@@ -81,9 +122,12 @@ int main(int argc, char **argv)
                 last_request = ros::Time::now();
             }
         }
-
-        local_pos_pub.publish(pose);
-
+        
+        if(checkEqualPose(poses[i%4])) {
+            i++;
+            ROS_INFO("i: %d",i);
+        }
+        local_pos_pub.publish(poses[i%4]);
         ros::spinOnce();
         rate.sleep();
     }
