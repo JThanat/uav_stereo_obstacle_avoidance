5c04f0cae900d1de2816fca39261356a9fa1f92a
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2018-04-17 13:23:57 +0700

    Add arming client

--- rotors_obstacle_avoidance/src/offb_circle.cpp
+++ rotors_obstacle_avoidance/src/offb_circle.cpp
@@ -26,6 +26,9 @@ int main(int argc, char **argv)
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
 
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
+
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
     
