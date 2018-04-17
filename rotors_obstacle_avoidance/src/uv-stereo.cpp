b7b4385f0fc6782a2d5c15a3c3d24bbc87a34f3c
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2018-04-17 16:00:50 +0700

    Remove image codec

--- rotors_obstacle_avoidance/src/uv-stereo.cpp
+++ rotors_obstacle_avoidance/src/uv-stereo.cpp
@@ -14,7 +14,7 @@ v                     v
 #include <opencv2/videoio.hpp>
 
 #include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/imgcodecs.hpp>
+// #include <opencv2/imgcodecs.hpp>
 #include <opencv2/core/utility.hpp>
 
 #include <iostream>
