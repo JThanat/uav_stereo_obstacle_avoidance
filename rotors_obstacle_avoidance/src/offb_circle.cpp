0b82fe5cb9d8bcbb93e1cea3990ebb58156eddbc
JThanat <thanat.jat@gmail.com>
2018-04-23 14:16:55 +0700

    Change offb_circle

--- rotors_obstacle_avoidance/src/offb_circle.cpp
+++ rotors_obstacle_avoidance/src/offb_circle.cpp
@@ -1,8 +1,11 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/GlobalPositionTarget.h>
+#include <sensor_msgs/NavSatFix.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <sensor_msgs/NavSatFix.h>
 #include <vector>
 #include <cmath>
 
@@ -13,25 +16,26 @@ double theta;
 double count=0.0;
 double wn;
 
+typedef mavros_msgs::GlobalPositionTarget gbpos;
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
-geometry_msgs::PoseStamped current_pose;
-void pose_cb(const geometry_msgs::PoseStamped::ConstPtr &msg)
+sensor_msgs::NavSatFix current_pose;
+void pose_cb(const sensor_msgs::NavSatFix::ConstPtr &msg)
 {
     current_pose = *msg;
 }
 
-bool checkEqualPose(const geometry_msgs::PoseStamped expectedPosition)
-{
-    return (
-        std::abs(expectedPosition.pose.position.x - current_pose.pose.position.x) < 0.3 && 
-        std::abs(expectedPosition.pose.position.y - current_pose.pose.position.y) < 0.3 && 
-        std::abs(expectedPosition.pose.position.z - current_pose.pose.position.z) < 0.3
-    );
-}
+// bool checkEqualPose(const geometry_msgs::PoseStamped expectedPosition)
+// {
+//     return (
+//         std::abs(expectedPosition.pose.position.x - current_pose.pose.position.x) < 0.3 && 
+//         std::abs(expectedPosition.pose.position.y - current_pose.pose.position.y) < 0.3 && 
+//         std::abs(expectedPosition.pose.position.z - current_pose.pose.position.z) < 0.3
+//     );
+// }
 
 int main(int argc, char **argv)
 {
@@ -40,66 +44,73 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+    ros::Publisher global_pos_pub = nh.advertise<mavros_msgs::GlobalPositionTarget>("mavros/setpoint_raw/global", 10);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
 
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
-    ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 10, pose_cb);
+    ros::Subscriber pose_sub = nh.subscribe<sensor_msgs::NavSatFix>("/mavros/global_position/global", 10, pose_cb);
+    
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
     
 
     // wait for FCU connection
-    while(ros::ok() && current_state.connected){
+    while(ros::ok() && !current_state.connected){
         ros::spinOnce();
         rate.sleep();
     }
 
-    std::vector<geometry_msgs::PoseStamped> poses(20);
-    poses[0].pose.position.x = 0;
-    poses[0].pose.position.y = 0;
-    poses[0].pose.position.z = 5;
-    
-    for (i = 0 ; i < 20 ; i++)
-    {
-        if(i <= 5)
-        {
-            poses[i].pose.position.x = i;
-            poses[i].pose.position.y = 0;
-            poses[i].pose.position.z = 5;
-        }
-        else if(i <= 10)
-        {
-            poses[i].pose.position.x = 5;
-            poses[i].pose.position.y = i - 5;
-            poses[i].pose.position.z = 5;
-        }
-        else if(i <= 15)
-        {
-            poses[i].pose.position.x = 15 - i;
-            poses[i].pose.position.y = 5;
-            poses[i].pose.position.z = 5;
-        }
-        else 
-        {
-            poses[i].pose.position.x = 0;
-            poses[i].pose.position.y = 20 - i;
-            poses[i].pose.position.z = 5;
-        }
-    }
+    std::vector<mavros_msgs::GlobalPositionTarget> poses(20);
+    geometry_msgs::PoseStamped local_pose;
+    local_pose.pose.position.x = 0;
+    local_pose.pose.position.y = 0;
+    local_pose.pose.position.z = 5;
 
+    poses[0].latitude = current_pose.latitude;
+    poses[0].longitude = current_pose.longitude;
+    poses[0].altitude = 5;
+    poses[0].velocity.x = 2.0;
+    poses[0].velocity.y = 2.0;
+    poses[0].velocity.z = 2.0;
+    poses[0].acceleration_or_force.x = 2;
+    poses[0].acceleration_or_force.y = 2;
+    poses[0].acceleration_or_force.z = 2;
+    poses[0].coordinate_frame = gbpos::FRAME_GLOBAL_REL_ALT;
+    poses[0].type_mask = gbpos::IGNORE_YAW_RATE + gbpos::IGNORE_YAW + gbpos::FORCE;
+
+    std::cout <<  poses[0].latitude << " " <<  poses[0].longitude << " " << poses[0].altitude << std::endl;
+
+    poses[1].latitude = current_pose.latitude;
+    poses[1].longitude = current_pose.longitude + 0.1;
+    poses[1].altitude = 5;
+    poses[1].velocity.x = 2.0;
+    poses[1].velocity.y = 2.0;
+    poses[1].velocity.z = 2.0;
+    poses[1].acceleration_or_force.x = 2;
+    poses[1].acceleration_or_force.y = 2;
+    poses[1].acceleration_or_force.z = 2;
+    poses[1].coordinate_frame = gbpos::FRAME_GLOBAL_REL_ALT;
+    poses[1].type_mask = gbpos::IGNORE_YAW_RATE + gbpos::IGNORE_YAW + gbpos::FORCE;
+
+
+    std::cout <<  poses[1].latitude << " " <<  poses[1].longitude << " " << poses[1].altitude << std::endl;
+    
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
-        local_pos_pub.publish(poses[0]);
+        global_pos_pub.publish(poses[0]);
         ros::spinOnce();
         rate.sleep();
     }
     mavros_msgs::SetMode offb_set_mode;
-    offb_set_mode.request.custom_mode = "OFFBOARD";
+    offb_set_mode.request.custom_mode = "GUIDED";
 
     mavros_msgs::CommandBool arm_cmd;
     arm_cmd.request.value = true;
+    
+    bool enabled = false;
+    bool armed = false;
 
     ros::Time last_request = ros::Time::now();
 
@@ -111,36 +122,49 @@ int main(int argc, char **argv)
         //     ROS_INFO("Vehicle is not in GUIDED Mode");
         //     continue;
         // }
-        if (current_state.mode != "OFFBOARD" &&
+        if (current_state.mode != "GUIDED" &&
             (ros::Time::now() - last_request > ros::Duration(5.0)))
         {
             if (set_mode_client.call(offb_set_mode) &&
                 offb_set_mode.response.mode_sent)
             {
-                ROS_INFO("Offboard enabled");
+                ROS_INFO("GUIDED enabled");
+                enabled = true;
             }
             last_request = ros::Time::now();
         }
-        else
+        else if(current_state.mode == "GUIDED")
         {
-            if (!current_state.armed &&
+            enabled = true;
+            if(current_state.armed)
+            {
+                armed = true;
+            }
+            else if (!current_state.armed &&
                 (ros::Time::now() - last_request > ros::Duration(5.0)))
             {
                 if (arming_client.call(arm_cmd) &&
                     arm_cmd.response.success)
                 {
                     ROS_INFO("Vehicle armed");
+                    armed = true;
                 }
                 last_request = ros::Time::now();
             }
 
         }
-        if(checkEqualPose(poses[i%20]))
+
+        if (!enabled || !armed)
         {
-            i++;
+            ROS_INFO("Pub 0");
+            global_pos_pub.publish(poses[0]);
+        } 
+        else
+        {
+            ROS_INFO("Pub 1");
+            global_pos_pub.publish(poses[1]);
         }
 
-        local_pos_pub.publish(poses[i%20]);
         ros::spinOnce();
         rate.sleep();
     }
